313605f702dd8b3820f496596c941215787c14d0
ian16munoz3nunez1 <ianemn1603@gmail.com>
2024-02-13 20:28:35 -0600

    MLP: El Perceptron Multicapa como clasificador
    Se desarrolló un código en Python y Octave para utilizar un
    Perceptrón Multicapa que sea capaz de clasificar las
    entradas de la compuerta XOR.

---
+++ mlpPred.m
@@ -0,0 +1,14 @@
+% Ian Mu;oz Nu;ez - MLP (Perceptron Multicapa)
+
+function yp = mlpPred(model, x)
+    w = model.w;
+    b = model.b;
+    L = length(w);
+    yp = x;
+
+    for l= 1:L-1
+        yp = tanh((w{l}'*yp) + b{l});
+    end
+    v = (w{L}'*yp) + b{L};
+    yp = 1./(1 + exp(-v));
+end
